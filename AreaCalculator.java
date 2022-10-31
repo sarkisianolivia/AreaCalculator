8cb0cceed7052587486147b1ce48f2733204316d
Olivia Sarkisian <115608329+sarkisianolivia@users.noreply.github.com>
2022-10-31 00:31:11 -0500

    Add files via upload

---
+++ AreaCalculator.java
@@ -0,0 +1,25 @@
+public class AreaCalculator {
+
+    public static double area (double radius) {
+        if (radius < 0) {
+            return -1.0;
+        }
+        double areaOfCircle = radius * radius * Math.PI;
+        return areaOfCircle;
+
+    }
+
+    public static double area (double x, double y) {
+        if (x < 0 || y < 0) {
+            return -1.0;
+        }
+
+        double areaOfRectangle = x * y;
+        return areaOfRectangle;
+
+
+    }
+
+
+
+}
